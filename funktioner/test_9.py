b8f562eb79d1b30f80ba4acccc1f0f113a364580
admin <admin@gmailcom>
2025-03-25 15:40:13 +0100

    test

---
+++ funktioner/test_9.py
@@ -0,0 +1,21 @@
+
+# Vad skrivs ut? Skriv som det ser ut i konsollen när koden nedan körs.
+def a():
+ print("b")
+def c():
+ return("d")
+def e(f):
+ return f * f
+def i(j):
+ print("a", j)
+def l(m):
+ return"m" + m
+def o(p):
+ return p + 2
+a()
+g = c()
+print(g)
+print(e(3))
+i("c")
+print(l("o"))
+print(o(4))
